fabc5b699925cf2702ad194a584494397b5e0310
rmoskal <rmoskal@mostmedia.com>
2019-05-02 17:09:43 +0000

    System commit

--- minimalinputfragment/CxEmployee.js
+++ minimalinputfragment/CxEmployee.js
@@ -2,13 +2,6 @@
 const CXEMPLOYEEINPUTMIN = `fragment CXEMPLOYEEINPUTMIN on CxEmployee {
   LastName
   FirstName
-  Person {
-    Id
-    Name
-  }
-  Owner {
-    Id
-  }
   ResourceSubtype {
     Id
   }
