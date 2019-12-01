ccf3d49f6b4353f1fa264d7ea78f9761aa1c2809
rmoskal <rmoskal@mostmedia.com>
2019-12-01 16:12:09 +0000

    System commit

---
+++ minimalinputfragment/CxConfigValue.js
@@ -0,0 +1,14 @@
+
+const CXCONFIGVALUEINPUTMIN = `fragment CXCONFIGVALUEINPUTMIN on CxConfigValue {
+  Key
+  Value
+  Id
+}`;
+module.exports = { fragment: CXCONFIGVALUEINPUTMIN,
+                   fragmentName: 'CXCONFIGVALUEINPUTMIN',
+                   query:'cxConfigValues',
+                   saveMutation :  'cxConfigValues_onSave',
+                   deletionMutation :  'cxConfigValues_onDelete',
+                   objectType: 'ConfigValue',
+                   clazz: 'CxConfigValueInput'
+                 };
