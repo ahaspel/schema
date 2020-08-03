b408cd445cffe5dc336b6978c0cc1a0e6585a039
rmoskal <rmoskal@mostmedia.com>
2020-08-03 19:39:08 +0000

    System commit

--- minimalinputfragment/CxUnit.js
+++ minimalinputfragment/CxUnit.js
@@ -1,1 +1,15 @@
 
+const CXUNITINPUTMIN = `fragment CXUNITINPUTMIN on CxUnit {
+  UnitType
+  Id
+  Name
+}`;
+module.exports = { fragment: CXUNITINPUTMIN,
+                   fragmentName: 'CXUNITINPUTMIN',
+                   query:'cxUnits',
+                   saveMutation :  'cxUnits_onSave',
+                   deletionMutation :  'cxUnits_onDelete',
+                   objectType: 'Unit',
+                   clazz: 'CxUnitInput',
+                   entityClass: 'CxUnit'
+                 };
