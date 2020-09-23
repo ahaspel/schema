32dac02fe2af10e565be73a87e40452e0a83169a
rmoskal <rmoskal@mostmedia.com>
2020-09-23 18:39:41 +0000

    System commit

---
+++ minimalinputfragment/CxAttribute.js
@@ -0,0 +1,16 @@
+
+const CXATTRIBUTEINPUTMIN = `fragment CXATTRIBUTEINPUTMIN on CxAttribute {
+  Id
+  Name
+  Value
+  AttributeId
+}`;
+module.exports = { fragment: CXATTRIBUTEINPUTMIN,
+                   fragmentName: 'CXATTRIBUTEINPUTMIN',
+                   query:'cxAttributes',
+                   saveMutation :  'cxAttributes_onSave',
+                   deletionMutation :  'cxAttributes_onDelete',
+                   objectType: 'Attribute',
+                   clazz: 'CxAttributeInput',
+                   entityClass: 'CxAttribute'
+                 };
